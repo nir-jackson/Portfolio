9a74e4b21f2e836405ca895b1c6b3142ee8ae3a4
Nir Jackson <nir.jackson89@gmail.com>
2017-04-22 14:41:13 +0300

    poi1

---
+++ public/poi1/js/poiatlocationcustome.js
@@ -0,0 +1,89 @@
+var jsonFile;
+
+
+// implementation of AR-Experience (aka "World")
+
+var World = {
+	// true once data was fetched
+	initiallyLoadedData: false,
+
+	// POI-Marker asset
+	markerDrawable_idle: null,
+
+	// called to inject new POI data
+	loadPoisFromJsonData: function loadPoisFromJsonDataFn() {
+        jsonFile = $.getJSON( "JsonFile.json", function() {
+          console.log( "success" );
+        })
+          .done(function() {
+            console.log( "second success" );
+          })
+          .fail(function() {
+            console.log( "error" );
+          })
+          .always(function() {
+            console.log( "complete" );
+          });
+
+        // Perform other work here ...
+
+        World.markerDrawable_idle = new AR.ImageResource("assets/marker_idle.png");
+        var markerLocation = new AR.GeoLocation( jsonFile.markers[0].lat, jsonFile.markers[0].long, 100.0);
+        var markerImageDrawable_idle = new AR.ImageDrawable(World.markerDrawable_idle, 2.5, {
+            zOrder: 0,
+            opacity: 1.0
+        });
+        var markerObject = new AR.GeoObject(markerLocation, {
+        			drawables: {
+        				cam: [markerImageDrawable_idle]
+        			}
+        		});
+
+
+
+
+        // Set another completion function for the request above
+        jsonFile.complete(function() {
+          console.log( "second complete" );
+        });
+
+
+		// Updates status message as a user feedback that everything was loaded properly.
+		World.updateStatusMessage('1 place loaded');
+	},
+
+	// updates status message shown in small "i"-button aligned bottom center
+	updateStatusMessage: function updateStatusMessageFn(message, isWarning) {
+
+		var themeToUse = isWarning ? "e" : "c";
+		var iconToUse = isWarning ? "alert" : "info";
+
+		$("#status-message").html(message);
+		$("#popupInfoButton").buttonMarkup({
+			theme: themeToUse
+		});
+		$("#popupInfoButton").buttonMarkup({
+			icon: iconToUse
+		});
+	},
+
+	// location updates, fired every time you call architectView.setLocation() in native environment
+	locationChanged: function locationChangedFn(lat, lon, alt, acc) {
+        //console.log("");
+		/*
+			The custom function World.onLocationChanged checks with the flag World.initiallyLoadedData if the function was already called. With the first call of World.onLocationChanged an object that contains geo information will be created which will be later used to create a marker using the World.loadPoisFromJsonData function.
+		*/
+		if (!World.initiallyLoadedData) {
+			// creates a poi object with a random location near the user's location
+
+			World.loadPoisFromJsonData();
+			World.initiallyLoadedData = true;
+		}
+	},
+};
+
+/* 
+	Set a custom function where location changes are forwarded to. There is also a possibility to set AR.context.onLocationChanged to null. In this case the function will not be called anymore and no further location updates will be received. 
+*/
+alert("ALERT!!!!!")
+AR.context.onLocationChanged = World.locationChanged;
